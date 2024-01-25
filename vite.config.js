be2f32de536255886161c641b82000fc75df8838
de-d <etzheded@gmail.com>
2024-01-25 11:34:38 +0300

    fix vite

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+
+export default defineConfig(async ({ command, mode }) => {
+    return {
+        build: {
+            cssCodeSplit: false,
+            outDir: "build"
+        },
+        server: {
+            host: "0.0.0.0",
+            port: 3000,
+            strictPort: true,
+            hmr: {
+                clientPort: 443
+            }
+        },
+    };
+});
